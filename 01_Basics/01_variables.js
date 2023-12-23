71d9c0d17195536735f3a05bac6e1debe1bf3273
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2023-12-23 19:28:14 +0000

    leraned datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553;
+let accountEmail = "gopishaw9191@hotmail.com";
+var accountPassword = "1234";
+accountCity = "Jaipur";
+let accountState;
+
+// accountId = 2 //not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "121212"
+accountCity = "Bengal";
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+    Prefer not to use var
+    beacuse of issue in block scope and 
+    functional scope
+*/
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name = "Gopi";
+let age = 110;
+let isLoggedIn = false;
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof age)
+console.log(typeof null)  // object
+console.log(typeof undefined) //undefined
