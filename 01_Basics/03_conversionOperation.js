8c4861848799726c957bef0d80624a30770371cd
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2023-12-25 08:10:41 +0000

    operations on data is done

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -19,6 +19,35 @@ console.log(booleanIsLoggedIn); // true
 // "" => false
 // "abc" => true
 
-let someNumber = 33
+let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
+
+// ************************** Opeartions ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" +2 ); // 12
+console.log(1 + "2"); // 12
+console.log("1" + "2"); // 12
+console.log( "1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); // 1
+console.log(+"") // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter ++; // post increment
+console.log(gameCounter);
+
