7362f943fe6fe58b7512408296d35be155059c84
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2023-12-26 18:27:36 +0000

    objects in js started

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,15 @@
+// singleton
+// Object.create()
+
+//object literrals
+const jsUser = {
+    name: "Gopi",
+    age: 21,
+    loc: "Raniganj",
+    email: "gopishaw111@gmail.com",
+    isLoggedIn: false,
+    lastLogin: ["Monday", "Saturday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"]);
