d58720a369128add5739817e519312fdc66453d4
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2023-12-26 18:41:42 +0000

    I will continue objects tomorrow

--- 02_Basics/03_objects.js
+++ 02_Basics/03_objects.js
@@ -2,8 +2,11 @@
 // Object.create()
 
 //object literrals
+const mySym = Symbol("key1")
 const jsUser = {
     name: "Gopi",
+    "full name": "Gopi Shaw",
+    [mySym]: "myKey1",
     age: 21,
     loc: "Raniganj",
     email: "gopishaw111@gmail.com",
@@ -13,3 +16,21 @@ const jsUser = {
 
 console.log(jsUser.email)
 console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+
+jsUser.email = "gopisha18072003@gmail.com"
+// Object.freeze(jsUser)
+jsUser.email = "chatgpt18072003@gmail.com"
+console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS user ${this.name}`);
+}
+
+console.log(jsUser.greeting); // undefined
+jsUser.greeting();
+jsUser.greetingTwo();
---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,5 @@
+const topperStudent = new Object();
+// singleton object
+
+const buffaloStudent = {};
+// non singleton student
